993deb1a7443fd4e1a494856c1c8947642b0675a
pengzhanbo <volodymyr@foxmail.com>
2026-06-27 15:02:02 +0800

    fix(vitepress-tuck): fix hook merging

--- packages/vitepress-tuck/client-types.d.ts
+++ packages/vitepress-tuck/client-types.d.ts
@@ -1,7 +1,8 @@
 declare module 'virtual:enhance-app' {
 
+  import type { Awaitable } from '@pengzhanbo/utils'
   import type { EnhanceAppContext } from 'vitepress/client'
 
-  const enhanceApp: (ctx: EnhanceAppContext) => void
+  const enhanceApp: (ctx: EnhanceAppContext) => Awaitable<void>
   export default enhanceApp
 }
