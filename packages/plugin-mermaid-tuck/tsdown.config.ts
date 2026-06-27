e9bd36bce60448651e90aac2c7ab45991979f645
pengzhanbo <volodymyr@foxmail.com>
2026-06-27 22:06:56 +0800

    chore: update tsdown config

--- packages/plugin-mermaid-tuck/tsdown.config.ts
+++ packages/plugin-mermaid-tuck/tsdown.config.ts
@@ -2,4 +2,7 @@ import { build } from '../../scripts/tsdown'
 
 export default build({
   styleAssets: ['virtual:tuck-icons.css'],
+  clientConfig: {
+    deps: { neverBundle: ['virtual:vitepress-mermaid'] },
+  },
 })
