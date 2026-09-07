3eb69b6f44b9336e525029d587d47f95419ba627
pengzhanbo <volodymyr@foxmail.com>
2026-09-07 12:03:58 +0800

    fix: add optional chaining operator to `import.meta.env`, close #55

--- packages/plugin-file-tree/src/client/constants.ts
+++ packages/plugin-file-tree/src/client/constants.ts
@@ -5,11 +5,11 @@ import type { InjectionKey, Ref } from 'vue'
  *
  * 文件树节点点击事件
  */
-export const ON_NODE_CLICK: InjectionKey<(filename: string, type: 'folder' | 'file') => void> = Symbol(import.meta.env.DEV ? 'on-file-tree-node-click' : '')
+export const ON_NODE_CLICK: InjectionKey<(filename: string, type: 'folder' | 'file') => void> = Symbol(import.meta.env?.DEV ? 'on-file-tree-node-click' : '')
 
 /**
  * Active file tree node
  *
  * 当前激活的文件树节点
  */
-export const ACTIVE_NODE_KEY: InjectionKey<Ref<string>> = Symbol(import.meta.env.DEV ? 'active-file-tree-node' : '')
+export const ACTIVE_NODE_KEY: InjectionKey<Ref<string>> = Symbol(import.meta.env?.DEV ? 'active-file-tree-node' : '')
